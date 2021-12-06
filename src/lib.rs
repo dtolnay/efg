2ed7fe58e6606cf3a9ee8174c95e933135da153f
David Tolnay <dtolnay@gmail.com>
2021-12-05 16:55:15 -0800

    Write crate-level doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+//! **Conditional compilation using boolean expression syntax, rather than
+//! *any()*, *all()*, *not()*.**
+//!
+//! <br>
+//!
+//! Rust's `cfg` and `cfg_attr` conditional compilation attributes use a
+//! restrictive domain-specific language for specifying configuration
+//! predicates. The syntax is described in the *[Conditional compilation]* page
+//! of the Rust reference. The reason for this syntax as opposed to ordinary
+//! boolean expressions was to accommodate restrictions that old versions of
+//! rustc used to have on the grammar of attributes.
+//!
+//! However, all restrictions on the attribute grammar were lifted in Rust
+//! 1.18.0 by [rust-lang/rust#40346]. This crate explores implementing
+//! conditional compilation using ordinary boolean expressions
+//! instead:&ensp;`&&`,&ensp;`||`,&ensp;`!`&ensp;as usual in Rust syntax.
+//!
+//! [Conditional compilation]: https://doc.rust-lang.org/1.57.0/reference/conditional-compilation.html
+//! [rust-lang/rust#40346]: https://github.com/rust-lang/rust/pull/40346
+//!
+//! <table>
+//! <tr><th><center>built into rustc</center></th><th><center>this crate</center></th></tr>
+//! <tr><td><code>#[cfg(any(<i>thing1</i>, <i>thing2</i>, &hellip;)]</code></td><td><code>#[efg(<i>thing1</i> || <i>thing2</i> || &hellip;)]</code></td></tr>
+//! <tr><td><code>#[cfg(all(<i>thing1</i>, <i>thing2</i>, &hellip;)]</code></td><td><code>#[efg(<i>thing1</i> &amp;&amp; <i>thing2</i> &amp;&amp; &hellip;)]</code></td></tr>
+//! <tr><td><code>#[cfg(not(<i>thing</i>))]</code></td><td><code>#[efg(!<i>thing</i>)]</code></td></tr>
+//! </table>
+//!
+//! <br>
+//!
+//! # Examples
+//!
+//! A real-world example from the `quote` crate:
+//!
+//! ```
+//! #[efg(feature = "proc-macro" && !(target_arch = "wasm32" && target_os = "unknown"))]
+//! extern crate proc_macro;
+//! ```
+//!
+//! and from the `proc-macro2` crate:
+//!
+//! ```
+//! #[efg(super_unstable || feature = "span-locations")]
+//! pub fn start(&self) -> LineColumn {
+//! ```
+
 mod error;
 mod expr;
 
